33d1de398957f41f1a3e05c8a28e316ef2234da7
Peter A. Higgins <pah@cenabo.com>
2018-07-13 13:08:48 +0100

    Add external db link - update

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host : 'postgresql-symmetrical-52512',
-        user : 'pah',
-        password : '',
-        database : 'smartbrain'
+        host : process.env.DATABASE_URL,
+        ssl: true
     }
 });
 
